0283110cf99b0602802b21bcdf650a31a6ededcd
Nils <nils.marten.lange@gmail.com>
2025-03-13 21:21:01 +0100

    included reading and processing of user setup data for nlsLM modeling

--- code/read_input.R
+++ code/read_input.R
@@ -41,3 +41,54 @@ get_compound_data <- function(residue_data) {
     )
     return(compound_data)
 }
+
+
+#' get setup data defined by user
+#'
+#' @return user_setup_data; string with setup data provided by user
+get_user_setup_data <- function() {
+    user_setup_data <- file.choose() |> 
+        readLines() |> 
+        (\(x) x[!grepl("^#", x)])() |>  # remove lines starting with #
+        trimws() |>                     # trim whitespace
+        (\(x) x[x != ""])()             # remove empty strings
+    return(user_setup_data)
+}
+
+
+#' process user setup data and structure and store it in a list
+#'
+#' @param user_setup_data string with setup data provided by user
+#' @return setup_data; list containing setup data relevant for nlsLM modeling
+create_setup_data <- function(user_setup_data) {
+    
+    setup_data <- list(
+        model_type = NULL,
+        start_parms = list(),
+        control_parms = list()
+    )
+    
+    for (line in user_setup_data) {
+        parts <- strsplit(line, "=")[[1]]
+        key <- trimws(parts[1])
+        value <- trimws(parts[2])
+        
+        if (value == "NA") {
+            value <- NA
+        } else if (grepl("^[0-9]+$", value)) { # check if it contains only numeric characters
+            value <- as.numeric(value)
+        } else {
+            value <- value
+        }
+        
+        if (key == "model_type") {
+            setup_data$model_type <- value
+        } else if (key %in% c("kp", "kp1", "kp2", "g", "km", "kfm")) {
+            setup_data$start_parms[[key]] <- value
+        } else if (key %in% c("maxiter", "tol", "minFactor", "ftol", "ptol", "gtol", "nprint")) {
+            setup_data$control_parms[[key]] <- value
+        }
+    }
+    
+    return(setup_data)
+}
