50293ff4e271702ce84b3a27ffef42868666d6b8
Nils <nils.marten.lange@gmail.com>
2025-03-26 22:30:26 +0100

    fixed plot code to handle multiple compartments

--- code/plot.R
+++ code/plot.R
@@ -4,7 +4,7 @@
 #' @return plot_data; dataframe containing observation
 create_plot_data <- function(fitting_data) {
     plot_data <- fitting_data %>%
-        select(time, observation)
+        select(time, compound, observation)
     return(plot_data)
 }
 
@@ -33,8 +33,12 @@ create_smooth_data <- function(model_type, reg_model, plot_data) {
     smooth_data <- data.frame(
         time = smooth_time,
         prediction = smooth_prediction
-    )
-    
+    ) %>%
+        mutate(compound = if (length(unique(plot_data$compound)) == 1) {
+            unique(plot_data$compound)
+        } else {
+            rep(unique(plot_data$compound), each = nrow(.) / 2)
+        })
     return(smooth_data)
 }
 
@@ -64,33 +68,36 @@ set_model_parms <- function(reg_model, model_func) {
 #' @param smooth_data dataframe containing prediction on a fine time scale
 #' @return fit_graph; plot with observation and prediction
 create_fit_graph <- function(model_type, plot_data, smooth_data) {
+
+    unique_compounds <- unique(plot_data$compound)
+    colors <- setNames(c("black", "darkgray", "black", "darkgray"), unique_compounds)
     
     fit_graph <- ggplot() +
         geom_point(data = plot_data, 
                    aes(x = time, 
                        y = observation, 
-                       color = "Observation"),
+                       color = compound),
                    size = 2) +
         geom_line(data = smooth_data, 
                   aes(x = time, 
                       y = prediction, 
-                      color = "Prediction"),
+                      color = compound),
                   linewidth = 1) +
-        scale_color_manual(values = c("Observation" = "black", "Prediction" = "darkgray")) +
+        scale_color_manual(values = colors) +
         labs(
             title = model_type,
             x = "Time",
             y = "Residue",
-            color = NULL
+            color = "Compound"
         ) +
         xlim(min(plot_data$time), max(plot_data$time)) +
         theme_minimal(base_size = 12) +
         theme(
-            legend.position = "top",
+            legend.position = ifelse(length(unique_compounds) > 1, "top", "none"), # hide legend for single compound
             legend.text = element_text(size = 10),
-            plot.title = element_text(hjust = 0.5, size = 14), # center the plot title
-            axis.title.x = element_text(margin = margin(t = 10)), # move x-axis title
-            axis.title.y = element_text(margin = margin(r = 10))  # move y-axis title
+            plot.title = element_text(hjust = 0.5, size = 14),
+            axis.title.x = element_text(margin = margin(t = 10)),
+            axis.title.y = element_text(margin = margin(r = 10))
         )
     
     return(fit_graph)
